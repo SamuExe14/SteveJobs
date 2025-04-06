1b0c5a270da4c2d277787ca7a09fcf5b994b610b
samu.exe <s.leonardi@keyover.it>
2025-04-06 17:07:51 +0200

    Update

--- Programmazione/Lezione/Blocco5.py
+++ Programmazione/Lezione/Blocco5.py
@@ -44,6 +44,38 @@ except Exception as _:
    print("Errore generico: ", _)
 finally:
    if 'file' in locals():
-      file.close()
+      file.close() # in qualsiasi caso chiude il file
 
-# print(locals())
+file = open("./res/dati.txt", "r")
+file.read() # legge l'intero contenuto del file in una singola stringa
+file.readline() # legge una riga alla volta
+file.readlines() # legge tutte le righe e le restituisce come lista di righe
+f.close() # non deve mai mancare
+
+#? ESEMPIO READ
+with open("./res/dati.txt", "r") as f:
+   contenuto = f.read()
+   print(contenuto)
+   f.close()
+with open("./res/dati.txt", "r") as f:
+   prima_riga = f.readline()
+   print("Prima riga: ", prima_riga)
+   f.close()
+with open("./res/dati.txt", "r") as f:
+   righe = f.readlines()
+   for riga in righe:
+      print(riga.strip())
+   f.close()
+with open("./res/dati.txt", "r") as f:
+   for riga in f:
+      print(riga.strip())
+   f.close()
+#? ESEMPIO WRITE
+
+with open("./res/dati.txt", "w") as f:
+   f.write("Questo è un esempio di scrittura su file")
+   f.close()
+
+linee = ["Prima riga\n", "Seconda riga\n", "Terza riga\n"]
+with open("./res/dati.txt", "w") as f:
+   f.writelines(linee)
